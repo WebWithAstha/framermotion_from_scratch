cb6d2d4690f9c7938d59de4b4fb886baecc7f046
WebWithAstha <lodhiastha85@gmail.com>
2024-08-01 21:53:42 +0530

    few change

--- src/components/AnimationControls.jsx
+++ src/components/AnimationControls.jsx
@@ -1,5 +1,5 @@
 import { motion, useAnimationControls} from 'framer-motion'
-import React from 'react'
+import React, { useState } from 'react'
 
 
 const change = {
@@ -10,22 +10,32 @@ const change = {
         borderRadius: "50%"
     }
 }
+
+
+
 const AnimationControls = () => {
 
     const control = useAnimationControls()
 
+    const [isClicked, setisClicked] = useState(false)
+
     const handleClick = () => {
-        control.start("round")
+        control.start({
+            scale:2
+        })
+        // iski style round
     }
 
     return (
         <div className='w-max h-max relative flex gap-[2vw] items-center p-[2vw]'>
-            <button onClick={handleClick} className='bg-neutral-300 w-max h-max px-[3vw] py-[1.2vw] text-[1.5vw]  font-semibold rounded uppercase'>box to circle</button>
+            {/* btn */}
+            <button onClick={e=>{handleClick;setisClicked(!isClicked)}} className='bg-neutral-300 w-max h-max px-[3vw] py-[1.2vw] text-[1.5vw]  font-semibold rounded uppercase'>box to circle</button>
+            {/* change */}
             <motion.div
                 variants={change}
                 initial="initial"
-                animate={control}
-                className="w-[5vw] h-[5vw] bg-rose-50"></motion.div>
+                animate={isClicked ? "round" :"initial"}
+                className="w-[5vw] h-[5vw] bg-rose-500"></motion.div>
         </div>
     )
 }
